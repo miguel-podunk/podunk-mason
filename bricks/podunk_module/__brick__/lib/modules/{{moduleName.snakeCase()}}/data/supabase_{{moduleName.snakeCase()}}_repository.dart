0fe626793de7ee1412876eb90188ec6db31ec713
Miguel <miguel@podunk.es>
2025-08-08 09:58:42 +0100

    fix typos

--- bricks/podunk_module/__brick__/lib/modules/{{moduleName.snakeCase()}}/data/supabase_{{moduleName.snakeCase()}}_repository.dart
+++ bricks/podunk_module/__brick__/lib/modules/{{moduleName.snakeCase()}}/data/supabase_{{moduleName.snakeCase()}}_repository.dart
@@ -1,5 +1,5 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:{{projectName.paramCase()}}/modules/{{moduleName.paramCase()}}/data/{{moduleName.snakeCase()}}_repository.dart'
+import 'package:{{projectName.paramCase()}}/modules/{{moduleName.paramCase()}}/data/{{moduleName.snakeCase()}}_repository.dart';
 
 class Supabase{{moduleName.pascalCase()}}Repository extends {{moduleName.pascalCase()}}Repository {
   final _supabase = Supabase.instance.client;
